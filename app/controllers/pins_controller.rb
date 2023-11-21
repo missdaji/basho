3909d09eb2a3d71208be8a3d5df1cbbad8f55edd
stephanie di dino <stephanie.didino@gmail.com>
2023-11-21 17:27:08 +0900

    Added new and create pin.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,11 +1,13 @@
 class PinsController < ApplicationController
   def new
     @pin = Pin.new
+    authorize @pin
   end
 
   def create
     @pin = Pin.new(pin_params)
     @pin.user = current_user
+    authorize @pin
     if @pin.save
       redirect_to pin_path(@pin)
     else
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,10 +2,6 @@ class Pin < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :address, presence: true
-  validates :comments, presence: true
-  validates :rating, presence: true
-  validates :visited, presence: true
-  validates :private, presence: true, default: false
 end
