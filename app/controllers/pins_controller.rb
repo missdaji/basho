559ab9308395e2838c7310943cb65547b4c5ca11
stephanie di dino <stephanie.didino@gmail.com>
2023-11-21 14:40:58 +0900

    Started working on new pin

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,21 @@
+class PinsController < ApplicationController
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    @pin.user = current_user
+    if @pin.save
+      redirect_to pin_path(@pin)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:picture_url, :name, :address, :longitude, :latitude, :comments, :icon, :rating, :visited, :private)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_015930) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_015834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,23 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_015930) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
+  create_table "pins", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.float "longitude"
+    t.float "latitude"
+    t.string "comments"
+    t.string "icon"
+    t.integer "rating"
+    t.boolean "visited"
+    t.boolean "private"
+    t.string "picture_url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,11 +68,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_015930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "pins", "users"
 end
