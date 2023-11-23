a77ec79cd5fd837743826d2b33c91d5c06e6f1bf
Julien Lesueur <julienlesueur@outlook.fr>
2023-11-23 17:22:52 +0900

    updated show and new pages

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,7 @@
 class Pin < ApplicationRecord
   acts_as_taggable_on :tags
+  ActsAsTaggableOn::Tag.most_used(10)
+  ActsAsTaggableOn::Tag.least_used(10)
   belongs_to :user
   has_one_attached :photo
 
