5b3e500771b973d99a441952ec896942c9ae0bba
Jonatan Jakobsson <jonatanjak@hotmail.no>
2023-11-29 15:08:30 +0900

    marker colors

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -15,6 +15,23 @@ class Pin < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  ICONS = {
+    'utensils' => "#E63946",
+    'ice-cream' => "#FDB9C8",
+    'burger' => "#FFD700",
+    'pizza-slice' => "#808000",
+    'carrot' => "#FFA500",
+    'mug-hot' => "#6B4423",
+    'martini-glass' => "#4E2A84",
+    'beer-mug-empty' => "#FFBF00",
+    'bag-shopping' => "#00BFFF",
+    'tree' => "#008000",
+    'landmark' => "#808080",
+    'book' => "#800000",
+    'music' => "#800080",
+    'gamepad' => "#00FF00",
+    'dice' => "#DC143C"
+  }
 
   def blank_stars
     5 - rating
