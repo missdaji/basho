4dca4e39d1de69da9107716c554647343cc4e045
stephanie di dino <stephanie.didino@gmail.com>
2023-11-29 17:56:07 +0900

    Fixed Google url.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
   def search
     if params[:query]
       query = params[:query]
-      url = "https://maps.googleapis.com/maps/api/place/textsearch/json?location=35.63409893106672%139.7080938092547&radius=90000&query=#{query}&language=en&key=AIzaSyCXTpimDWCsoKWxKUB6rBrKy--PZgNjOpo"
+      url = "https://maps.googleapis.com/maps/api/place/textsearch/json?location=35.63409893106672%139.7080938092547&radius=90000&query=#{query}&language=en&key=#{ENV['GOOGLE_API_KEY']}"
       uri = URI(url)
       response = Net::HTTP.get_response(uri)
       google_place_data = JSON.parse(response.body)
